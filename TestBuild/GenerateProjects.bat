e57c86422c35f586085489b53089100e0d882d01
Sammy Fatnassi <sammyfreg@gmail.com>
2022-08-07 16:17:21 +0900

    Update to GenerateProject batfile
    Changes to make it work properly in GitHub Actions

--- TestBuild/GenerateProjects.bat
+++ TestBuild/GenerateProjects.bat
@@ -1,2 +1,13 @@
+@echo off
+
+REM Launch Solution Generate
+REM Use "GenerateProjects.bat 1" to skip the key press waiting (usefull for CI)
+
+pushd %~dp0
 Build\Sharpmake\Sharpmake.Application.exe /sources(@'Build\main.sharpmake.cs') /verbose 
+popd
+
+IF "%1" == "1" GOTO end
 pause
+
+:end
